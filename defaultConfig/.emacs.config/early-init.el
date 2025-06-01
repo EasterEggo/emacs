5507e1fe4bf87049df250d59c4d8383656453c91
EasterEggo <phsb0607@tutanota.com>
2025-06-01 12:26:27 -0300

    added JetBraisMono Nerd Font and black background in startup

--- defaultConfig/.emacs.config/early-init.el
+++ defaultConfig/.emacs.config/early-init.el
@@ -1,1 +1,5 @@
+(add-to-list 'default-frame-alist
+             '(background-color . "#000000")
+             '(foreground-color . "#FFFFFF"))
+(set-face-attribute 'default nil :family "JetBrainsMono Nerd Font" :height 110)
 (setq package-enable-at-startup nil)

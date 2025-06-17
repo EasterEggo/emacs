7e8840596c00ae8915437fbab740133a6c136132
EasterEggo <phsb0607@tutanota.com>
2025-06-16 21:50:59 -0300

    made cosmetic changes, added note taking related things and moved to eat instead of vterm

--- defaultConfig/.emacs.config/early-init.el
+++ defaultConfig/.emacs.config/early-init.el
@@ -1,6 +1,8 @@
+;; -*- lexical-binding: t; -*-
 (setenv "LSP_USE_PLISTS" "true")
-(add-to-list 'default-frame-alist
-             '(background-color . "#000000")
-             '(foreground-color . "#FFFFFF"))
-(set-face-attribute 'default nil :family "JetBrainsMono Nerd Font" :height 110)
+(add-to-list 'initial-frame-alist '(background-color . "#1d1f21"))
+(custom-theme-set-faces
+ 'user
+ '(variable-pitch ((t (:family "aporetic-sans-normal" :height 130 :weight regular))))
+ '(fixed-pitch ((t ( :family "aporetic-serif-mono" :height 130)))))
 (setq package-enable-at-startup nil)
--- defaultConfig/.emacs.config/init.el
+++ defaultConfig/.emacs.config/init.el
@@ -1,6 +1,6 @@
 ;init.el -*- lexical-binding: t; -*- 
+(setq native-comp-deferred-compilation t)
 (setq inhibit-startup-screen t)
-(global-display-line-numbers-mode 1)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
@@ -10,13 +10,13 @@
 (electric-indent-mode 1)
 (which-key-mode 1)
 (savehist-mode 1)
+(setq pixel-scroll-mode 1)
 (setq electric-pair-pairs
       '(
         (?\" . ?\")
         (?\{ . ?\})))
 (setq-default tab-width 4)
 (setq-default tab-always-indent t)
-
 (defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
@@ -63,42 +63,35 @@
 (use-package general
   :ensure (:wait t))
 
-(use-package rg)
-
-(use-package doom-themes
-  :demand t
-  :config
-  (setq doom-themes-enable-bold t
-        doom-themes-enable-italic t)
-  (load-theme 'doom-gruvbox 1)
-  (doom-themes-visual-bell-config)
-  (doom-themes-neotree-config)
-  (doom-themes-org-config))
-
+;(use-package modus-themes
+;  :config
+;  (load-theme 'modus-vivendi t))
+(use-package ef-themes
+  :init
+  (mapc #'disable-theme custom-enabled-themes)
+  (load-theme 'ef-dream t))
 (use-package doom-modeline
-  :ensure t
   :init (doom-modeline-mode 1))
 
 (use-package nerd-icons
-  :after doom-modeline
-  :ensure t)
+  :after doom-modeline)
 
 (use-package rainbow-delimiters
-  :config
-  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
 
 (use-package evil
-  :hook (elpaca-after-init-hook . evil-mode))
+  :config
+  (evil-mode 1))
 (use-package evil-nerd-commenter
   :after evil
   :general(:states 'normal
 				   "<SPC>;" 'evilnc-comment-or-uncomment-lines))
 
 (use-package vertico
-  :commands (execute-extended-command)
-  :general
-  (:states 'normal :prefix "<SPC>"
-		"<SPC>" 'execute-extended-command)
+  :general (:states 'normal :prefix "<SPC>"
+					"." 'find-file
+					"<SPC>" 'execute-extended-command)
   :custom
   (vertico-cycle t)
   :init
@@ -113,16 +106,14 @@
               ("M-DEL" . vertico-directory-delete-word))
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 (use-package consult
-  :general
-  (:states 'normal
-		   :prefix "<SPC>"
-		   "sd" 'consult-find
-		   "sg" 'consult-ripgrep
-		   "sr" 'consult-recent-file
-		   "sB" 'consult-bookmark
-		   "sb" 'consult-buffer
-		   "st" 'consult-theme
-		   "sl" 'consult-line))
+  :general (:states 'normal :prefix "<SPC>"
+					"sd" 'consult-find
+					"sg" 'consult-ripgrep
+					"sr" 'consult-recent-file
+					"sB" 'consult-bookmark
+					"sb" 'consult-buffer
+					"st" 'consult-theme
+					"sl" 'consult-line))
 
 (use-package projectile
   :after evil
@@ -134,11 +125,20 @@
   :after evil
   :custom
   (corfu-auto t)
-  (corfu-quit-no-match 'separator)
   (corfu-cycle t)
   (corfu-preselect 'prompt)   
   :init
-  (global-corfu-mode))
+  (global-corfu-mode)
+  :config
+  (setq corfu-auto t
+      corfu-auto-delay  0.1
+      corfu-auto-prefix 0.1
+      corfu-quit-no-match t)
+  (add-hook 'corfu-mode-hook
+			(lambda ()
+              (setq-local completion-styles '(basic)
+                          completion-category-overrides nil
+                          completion-category-defaults nil))))
 (use-package cape
   :after corfu
   :init
@@ -160,20 +160,28 @@
 
 (use-package dashboard
   :config
+  (setq dashboard-startup-banner '(expand-file-name ("~/emacs/defaultConfig/.emacs.config/dash.txt")))
   (setq dashboard-display-icons-p t)
+  (setq dashboard-center-content t)
+  (setq dashboard-vertically-center-content t)
   (setq dashboard-icon-type 'nerd-icons)
   (setq dashboard-set-heading-icons t)
+  (setq dashboard-startupify-list '(dashboard-insert-banner
+									dashboard-insert-navigator
+									dashboard-insert-items
+									dashboard-insert-init-info
+									dashboard-insert-newline
+									dashboard-insert-footer))
   (setq dashboard-set-file-icons t)
   (setq dashboard-projects-backend 'projectile)
   (setq dashboard-items '((recents   . 5)
                           (bookmarks . 5)
                           (projects  . 5)
-                          (agenda    . 5)
-                          (registers . 5)))
+                          (agenda    . 5)))
   (add-hook 'elpaca-after-init-hook #'dashboard-insert-startupify-lists)
   (add-hook 'elpaca-after-init-hook #'dashboard-initialize)
   (dashboard-setup-startup-hook))
-(setq initial-buffer-choice (lambda () (get-buffer-create dashboard-buffer-name)))
+  (setq initial-buffer-choice (lambda () (get-buffer-create dashboard-buffer-name)))
 (use-package transient)
 (use-package magit
   :after transient
@@ -199,9 +207,12 @@
 (use-package centaur-tabs
   :hook
   (dashboard-mode . centaur-tabs-local-mode)
+  (eat-mode . centaur-tabs-local-mode)
   (term-mode . centaur-tabs-local-mode)
   (calendar-mode . centaur-tabs-local-mode)
   (org-agenda-mode . centaur-tabs-local-mode)
+  (elfeed-show-mode . centaur-tabs-local-mode)
+  (dired-mode . centaur-tabs-local-mode)
   :config
   (setq centaur-tabs-set-icons t)
   (setq centaur-tabs-icon-type 'nerd-icons)
@@ -225,13 +236,13 @@
 (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
   "Prepend emacs-lsp-booster command to lsp CMD."
   (let ((orig-result (funcall old-fn cmd test?)))
-    (if (and (not test?)                             ;; for check lsp-server-present?
-             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+    (if (and (not test?)                             
+             (not (file-remote-p default-directory))
              lsp-use-plists
-             (not (functionp 'json-rpc-connection))  ;; native json-rpc
+             (not (functionp 'json-rpc-connection))
              (executable-find "emacs-lsp-booster"))
         (progn
-          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+          (when-let ((command-from-exec-path (executable-find (car orig-result))))
             (setcar orig-result command-from-exec-path))
           (message "Using emacs-lsp-booster for %s!" orig-result)
           (cons "emacs-lsp-booster" orig-result))
@@ -244,11 +255,11 @@
   (lsp-mode . lsp-ui-mode)
   :config
   (evil-define-key 'normal lsp-mode-map (kbd "<SPC>c") lsp-command-map))
-(use-package lsp-ui :after lsp-mode :commands lsp-ui-mode)
+(use-package lsp-ui :commands lsp-ui-mode)
 
 (use-package denote
-  :general(:states 'normal
-				   :prefix "<SPC>n"
+  :general(:states 'normal :prefix "<SPC>o"
+				   "a" 'org-agenda
 				   "n" 'denote
 				   "r" 'denote-rename-file
 				   "l" 'denote-link
@@ -264,11 +275,10 @@
   :commands ( denote-journal-new-entry
               denote-journal-new-or-existing-entry
               denote-journal-link-or-create-entry)
-  :general
-  (:states 'normal :prefix "<SPC>nj"
-		   "j" 'denote-journal-new-entry
-		   "e" 'denote-journal-new-or-existing-entry
-		   "l" 'denote-journal-link-or-create-entry)
+  :general (:states 'normal :prefix "<SPC>oj"
+					"j" 'denote-journal-new-entry
+					"e" 'denote-journal-new-or-existing-entry
+					"l" 'denote-journal-link-or-create-entry)
   :config
   (add-hook 'calendar-mode-hook #'denote-journal-calendar-mode)
   (setq denote-journal-directory
@@ -279,9 +289,53 @@
   :commands (consult-denote-find
 			 consult-denote-grep)
   :general
-  (:states 'normal :prefix "<SPC>nf"
+  (:states 'normal :prefix "<SPC>of"
 		   "f" 'consult-denote-find
 		   "g" 'consult-denote-grep)
   :config
   (consult-denote-mode 1))
-(setq org-agenda-files '("~/Documents/agenda.org"))
+(setq org-agenda-files '(
+	  "~/Documents/orgAgenda/habits.org"
+	  "~/Documents/orgAgenda/personal.org"
+	  "~/Documents/orgAgenda/habits.org"))
+
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(add-hook 'text-mode-hook 'display-line-numbers-mode)
+(add-hook 'org-mode-hook 'org-indent-mode)
+(setq org-hide-emphasis-markers t)
+(use-package ein
+  :commands (ein:run
+			 ein:login))
+(use-package pdf-tools
+  :commands (pdf-loader-install)
+  :config
+  (pdf-loader-install)
+  (add-hook 'pdf-view-mode-hook (lambda() (display-line-numbers-mode -1))))
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
+(use-package nerd-icons-dired
+  :hook (dired-mode . nerd-icons-dired-mode))
+
+(use-package elfeed
+  :general (:states 'normal
+					"<SPC>ff" 'elfeed
+					"<SPC>fu" 'elfeed-update))
+(use-package elfeed-org
+  :config
+  (elfeed-org)
+  (setq rmh-elfeed-org-files (list "~/Documents/feeds.org")))
+(use-package eat
+  :ensure
+  '(:type git
+       :host codeberg
+       :repo "akib/emacs-eat"
+       :files ("*.el" ("term" "term/*.el") "*.texi"
+               "*.ti" ("terminfo/e" "terminfo/e/*")
+               ("terminfo/65" "terminfo/65/*")
+               ("integration" "integration/*")
+               (:exclude ".dir-locals.el" "*-tests.el")))
+
+  :general (:states 'normal
+					"<SPC>t" 'eat-other-window)
+  :config
+  (add-hook 'eshell-load-hook 'eat-eshell-mode)
+  (add-hook 'eshell-load-hook 'eat-eshell-visual-command-mode))
